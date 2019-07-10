b2c24818c0d10b3d754da99ea77f0efe6713de1d
Derek <dmikey@users.noreply.github.com>
2019-07-09 17:59:37 -0700

    Update index.js

--- src/components/SideBarItem/index.js
+++ src/components/SideBarItem/index.js
@@ -1,7 +1,7 @@
 var xtag = require('x-tag');
 var $ = require('zepto');
 
-module.exports = module.exports = xtag.register('x-side-bar-item', {
+module.exports = xtag.register('x-side-bar-item', {
   content: require('./template.html'),
   lifecycle: {
     inserted: function () {
